fc234896b757f1e6d2f5f7cca7941b0ad3e988ce
camaydesilvaa <camay.desilva@cdpipelinedevshop.com>
2022-08-01 15:56:47 +0000

    first commit

---
+++ head/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header.js';
+import './App.css';
+import Sidebar from './components/Sidebar.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
